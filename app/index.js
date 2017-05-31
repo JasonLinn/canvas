c7451e94e2190199c993c27a3f6009a009046975
JasonLinn <cheng11220@gmail.com>
2017-05-31 19:02:11 +0800

    webpack

--- app/index.js
+++ app/index.js
@@ -1,1 +1,46 @@
-console.log('123');
+// import '../sass/main.scss';
+import './css/style.css';
+//抓到canvas畫布
+const canvas = document.getElementById('canvas')
+//規劃2D繪圖環境
+/*======================
+        方塊
+=======================*/
+const context = canvas.getContext("2d")
+context.fillRect(100,100,250,200)
+context.strokeRect(200, 200, 300, 300)
+context.rect(400,400,50,50)
+context.stroke()
+/*======================
+　　      畫線
+=======================*/
+context.strokeStyle='red'
+context.lineWidth='50';
+context.moveTo(200,200);
+context.lineTo(300, 500);
+context.stroke();
+/*======================
+    　　陰影文字
+=======================*/
+context.shadowOffsetX = 5;
+context.shadowOffsetY = 5;
+context.shadowColor = 'rgba(255,0,0,0.8)';
+context.shadowBlur = 5;
+context.font = 'bold 50px Tahoma';
+context.fillStyle = 'steelblue';
+context.strokeStyle = 'deeppink';
+//填滿文字
+context.fillText('Hello World!', 200, 200);
+/*======================
+　　
+=======================*/
+var pic = new Image();
+pic.src = "./images/pug.jpg";
+pic.addEventListener('load', function () {
+    context.drawImage(pic, 500, 500, 100, 100)
+}, false);
+// context.fill();
+// context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 // 這邊使用 HtmlWebpackPlugin，將 bundle 好的 <script> 插入到 body。${__dirname} 為 ES6 語法對應到 __dirname  
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var node_modules = path.resolve(__dirname, "node_modules");
+const extractCss = new ExtractTextPlugin('app/css/[name].css');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const sourcePlugin = new HtmlWebpackInlineSourcePlugin();
 const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
     template: `${__dirname}/app/index.html`,
     filename: 'index.html',
@@ -9,30 +14,83 @@ const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
 
 module.exports = {
     // 檔案起始點從 entry 進入，因為是陣列所以也可以是多個檔案
-    entry: [
-        './app/index.js',
-    ],
+    entry: {
+        index:'./app/index.js',
+    }
+        
+    ,
     // output 是放入產生出來的結果的相關參數
     output: {
         path: `${__dirname}/dist`,
-        filename: 'index_bundle.js',
+        filename: '[name].js',
     },
     module: {
         // loaders 則是放欲使用的 loaders，在這邊是使用 babel-loader 將所有 .js（這邊用到正則式）相關檔案（排除了 npm 安裝的套件位置 node_modules）轉譯成瀏覽器可以閱讀的 JavaScript。preset 則是使用的 babel 轉譯規則，這邊使用 react、es2015。若是已經單獨使用 .babelrc 作為 presets 設定的話，則可以省略 query
-        loaders: [{
+        rules: [{
             test: /\.js$/,
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
                 presets: ['es2015', 'react'],
             },
-        }, ],
+        },
+        {
+            test: /\.css$/,
+            use: [{
+
+                loader: 'style-loader'
+
+            }, {
+
+                loader: 'css-loader?importLoaders=1'
+
+            }, {
+                loader: 'postcss-loader',
+                options: {
+                    plugins: function () {
+                        //自動加前綴
+                        return [require('autoprefixer')
+
+                        ];
+                    }
+                }
+            }]
+            },
+            {
+                test: /\.scss$/,
+                use: extractCss.extract({
+                    fallback: 'style-loader',
+                    //resolve-url-loader may be chained before sass-loader if necessary 
+                    use: [{
+                        loader: 'css-loader'
+                    }, {
+                        //讓sass加入前缀詞
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function () {
+                                return [require('autoprefixer')
+
+                                ];
+                            }
+                        }
+                    }, {
+                        loader: 'sass-loader'
+                    }]
+                })
+            }, {
+            test: /\.(svg|png|jpg|gif)$/,
+            use: [
+                //limit可以限制檔案大小到多大的時候啟用
+                { loader: 'url-loader?limit=500&mimetype=images/jpg&name=images/[name].[ext]' }
+            ]
+        } ],
     },
     // devServer 則是 webpack-dev-server 設定
     devServer: {
+        contentBase: "./dist",
         inline: true,
         port: 8008,
     },
     // plugins 放置所使用的外掛
-    plugins: [HTMLWebpackPluginConfig],
+    plugins: [HTMLWebpackPluginConfig, extractCss, sourcePlugin],
 };
